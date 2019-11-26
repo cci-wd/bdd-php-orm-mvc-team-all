a7a25b1258c98c77059d18aba9bcbfffdc343dd6
Tydaax <imtydax@gmail.com>
2019-11-26 16:34:05 +1100

    feat(platform) #42 #13 Install doctrine + Students CRUD

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Students;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $students = new Students();
+            $students->setName('John Doe');
+            $students->setAge(mt_rand(18, 40));
+            $students->setLocation('c koi sa');
+            $students->setPhoneNumber('123456');
+            $students->setEmail('test@test.test');
+            $manager->persist($students);
+        }
+
+        $manager->flush();
+    }
+}
