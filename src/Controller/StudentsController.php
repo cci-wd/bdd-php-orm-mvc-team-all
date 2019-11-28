b4f5f9d9da6db265e0f5616fdfab7b7811c37e12
soanakolokimoana <48263792+soanakolokimoana@users.noreply.github.com>
2019-11-29 10:38:36 +1100

    feat(product) : #98 add filters communes and sections

--- src/Controller/StudentsController.php
+++ src/Controller/StudentsController.php
@@ -2,15 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\City;
 use App\Entity\Offers;
-use App\Entity\Sections;
-use App\Entity\Businesses;
-use App\Form\StudentsType;
 use App\Entity\Skills;
+use App\Entity\Sections;
 use App\Entity\Students;
+use App\Entity\Businesses;
 use App\Entity\Educations;
-use App\Entity\Experiences;
+use App\Form\StudentsType;
 
+use App\Entity\Experiences;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -107,45 +108,50 @@ class StudentsController extends AbstractController
      */
     public function offers(Request $request): Response
     {
-        //$user = $this->getUser();
-        //$student = $user->student;
-        $offers = $this->getDoctrine()
-            ->getRepository(Offers::class)
-            ->findAll();
-
         $sections = $this->getDoctrine()
             ->getRepository(Sections::class)
+            ->findAll();
+
+        $cities = $this->getDoctrine()
+            ->getRepository(City::class)
             ->findAll();
         
         $keyword = $request->query->get("keyword");
         $location = $request->query->get("location");
         $section = $request->query->get("section");
 
-        if($keyword != "") {
-            $entityManager = $this->getDoctrine()->getManager();
-            $queryBuilder = $entityManager->createQueryBuilder();
-            $queryBuilder->select('o')
-                ->from(Offers::class, 'o')
-                ->where('o.title LIKE :keyword')
-                ->orWhere('o.description LIKE :keyword')
+        $entityManager = $this->getDoctrine()->getManager();
+        $queryBuilder = $entityManager->createQueryBuilder();
+        $queryBuilder
+            ->select('o')
+            ->from(Offers::class, 'o')
+            ->where('o.title LIKE :keyword')
+            ->setParameter('keyword', '%'.$keyword.'%');
 
-                ->setParameter('keyword', '%'.$keyword.'%');
-              
-            $query = $queryBuilder->getQuery();
-            $offers = $query->getResult();
-        } else {
-            $offers = $this->getDoctrine()
-                ->getRepository(Offers::class)
-                ->findAll();
+        if($location != "Toutes les communes") {
+            $queryBuilder->andWhere('o.location = :location')
+                ->setParameter('location', $location);
         }
 
+        if($section != "Toutes les sections") {
+            $queryBuilder
+                ->leftJoin('o.sections', 'sections')
+                ->andWhere('sections.name = :section')
+                ->setParameter('section', $section);
+        }
+    
+        $query = $queryBuilder->getQuery();
+        $offers = $query->getResult();
+        
         return $this->render('students/offers.html.twig', [
-            //'student' => $student,
             'offers'=> $offers,
+            'parameters'=>[
+                'keyword'=> $keyword,
+                'location'=>$location,
+                'section'=> $section,
+            ],
+            'cities'=> $cities,
             'sections'=> $sections,
-            'keyword'=> $keyword,
-            'location'=> $location,
-            'section'=> $section,
             'meta_title' => "Liste des offres"
         ]);
     }
