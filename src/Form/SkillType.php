e47e336740d58c3c9680d5e0bd629b6ca94b6fab
lorry <l.biret@cci-formation.nc>
2019-12-03 08:48:28 +1100

    fix(engineering):#82 fix in progress

---
+++ src/Form/SkillType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Skill;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class SkillsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'attr' => [ 'class' => 'form-control', 'type' => 'text', 'id' => '69']
+            ])
+            ->add('percentage', TextType::class, [
+                'attr' => [ 'class' => 'form-control', 'type' => 'text']
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Skill::class,
+        ]);
+    }
+}
