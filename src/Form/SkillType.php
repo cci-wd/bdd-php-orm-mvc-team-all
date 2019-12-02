4e56c175795d7b72f80c096f1e0a8809b2931c9e
lorry <l.biret@cci-formation.nc>
2019-12-03 09:22:26 +1100

    fix(engineering):#82 change SkillsType to SkillType

--- src/Form/SkillType.php
+++ src/Form/SkillType.php
@@ -9,7 +9,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class SkillsType extends AbstractType
+class SkillType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
